d22a1dd86c66ba2c7943b17d63c55377db7ec7ad
Bolun <36321182+BolunDai0216@users.noreply.github.com>
2022-12-10 11:23:05 -0500

    updated docs for getDataPath

--- B1Env/__init__.py
+++ B1Env/__init__.py
@@ -3,7 +3,7 @@ import os
 
 def getDataPath():
     """
-    Get the path where the package is installed
+    Get the path where the package is installed. This can be used to search for other file in this repo.
 
     :return: installation path.
     :rtype: str
