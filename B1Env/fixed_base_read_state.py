c1e6b3856f7a97dc649eacc5896ec01ecb0520cd
Bolun Dai <bd1555@nyu.edu>
2023-02-14 15:55:02 -0500

    minor changes

--- B1Env/fixed_base_read_state.py
+++ B1Env/fixed_base_read_state.py
@@ -1,9 +1,11 @@
 import numpy as np
+import pinocchio as pin
 import pybullet as p
 from bullet_utils.env import BulletEnvWithGround
 
 from B1Env.b1_sdk_api import B1SDKAPI
 from B1Env.b1wrapper import B1Robot
+from B1Env.config import B1Config
 
 
 def main():
@@ -25,7 +27,18 @@ def main():
     # Create connection api to the real robot
     robot_api = B1SDKAPI()
 
+    # Retrieve nominal configuration
+    q0 = np.matrix(B1Config.initial_configuration).T
+    dq0 = np.matrix(B1Config.initial_velocity).T
     com_pos = np.array([0.0, 0.0, 1.0])
+
+    # Leg ground reaction force
+    leg_gc_force = -np.array([[0.0], [0.0], [B1Config.mass * 9.81 / 4]])
+
+    kp = 10.0
+    kd = 1.0
+
+    count = 0
 
     # Read B1 state and show in simulator
     while True:
@@ -41,7 +54,18 @@ def main():
 
         # compute torque for gravity compensation of the legs
         _gravity = robot.pin_robot.gravity(q)
-        tau = _gravity[6:]
+        tau = (
+            _gravity[6:]
+            + kp * (np.asarray(q0)[7:, 0] - q[7:])
+            + kd * (np.asarray(dq0)[6:, 0] - dq[6:])
+        )
+
+        for idx in [robot.fr_index, robot.fl_index, robot.hr_index, robot.hl_index]:
+            _J = robot.pin_robot.getFrameJacobian(
+                idx, pin.ReferenceFrame.LOCAL_WORLD_ALIGNED
+            )[:3, 6:]
+            _tau_gc = _J.T @ leg_gc_force
+            tau = tau + _tau_gc[:, 0]
 
         # set desired joint position and velocity to zero
         q_cmd = np.zeros(12)
@@ -51,7 +75,13 @@ def main():
         robot_api.send_joint_command(q_cmd, dq_cmd, tau)
 
         # Step the simulator.
-        env.step(sleep=True)
+        env.step(sleep=False)
+        count += 1
+        print(count)
+
+        if count >= 20000:
+            kp = 60.0
+            kd = 6.0
 
 
 if __name__ == "__main__":
